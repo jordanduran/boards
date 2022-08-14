f5d874c7efa0e233547996307141d0b47b62329b
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2022-08-14 14:43:23 -0400

    Object scale-down complete skateboards

--- src/components/pages/complete-skateboards-page.jsx
+++ src/components/pages/complete-skateboards-page.jsx
@@ -27,11 +27,11 @@ const CompleteSkateboardsPage = () => {
                 to={`/complete/${complete.id}/${complete.name.split(' ')[0]}`}
                 className='group'
               >
-                <div className='w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-10'>
+                <div className='w-full aspect-w-1 aspect-h-1 bg-white rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-10'>
                   <img
                     src={complete.imageSrc}
                     alt={complete.name}
-                    className='w-full h-full object-center object-cover group-hover:opacity-75'
+                    className='w-full h-full object-center object-scale-down group-hover:opacity-75'
                   />
                 </div>
                 <h3 className='mt-4 text-sm text-gray-700 text-center capitalize'>
